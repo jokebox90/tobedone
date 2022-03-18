52c1e2b1eabfee659c252831fcbec344d146de9f
Jonathan Vagnier <adm.jvagnier@gmail.com>
2022-03-18 23:03:00 +0100

    Configure de Webpack et UiKit

--- assets/app.js
+++ assets/app.js
@@ -5,8 +5,9 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import './scss/app.scss';
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
 
-// start the Stimulus application
-import './bootstrap';
+// loads the Icon plugin
+UIkit.use(Icons);
